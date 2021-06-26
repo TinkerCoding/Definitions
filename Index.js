80044ac42d5d8526eef0bab1106782b7513a02b5
TinkerCoding <75763994+TinkerCoding@users.noreply.github.com>
2021-06-26 17:23:42 -0500

    Delete Index.js

--- Index.js
+++
@@ -1,3 +0,0 @@
-Index.js typically handles your app start up, routing and other functions of your applications and does require other modules to add functionality. If you're running
-a website or web app it would also handle become a basic HTTP web server replacing the role of something more traditional like Apache.
-
