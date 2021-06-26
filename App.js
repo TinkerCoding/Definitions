f7c84435ad5d55663e829acb529bb83550b32d39
TinkerCoding <75763994+TinkerCoding@users.noreply.github.com>
2021-06-26 17:34:53 -0500

    Create App.js

---
+++ App.js
@@ -0,0 +1,11 @@
+//The app.js file is usually the initial file to a Node.js app. It can have another name as well, as server.js, which is also very common.
+//The location should be your app root folder. The folder node modules is specific to libraries installed with npm. It doesn't make sense to have your app.js there.
+//I think you should place your app.js file in the root folder and adapt it to what the tutorial says. Also, the github page to create a basic app.
+
+
+
+//The goal of App.js is to provide a robust starting point for mobile webapps, handling general scenarios, and maintaining compatibility with other common JavaScript
+//libraries.
+
+
+
